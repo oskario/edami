0be625dd93e712f1bdc146382cd1466a1258eabd
oskario <oskario2007@gmail.com>
2015-06-14 18:29:09 +0200

    DBSCAN implemented

---
+++ src/main/scala/DBSCAN.scala
@@ -0,0 +1,87 @@
+import DBSCAN._
+
+import scala.collection.mutable.{ListBuffer, Set => MutableSet}
+
+object DBSCAN {
+
+  def apply[A](
+                data: Seq[A],
+                getNeighbours: (Point[A], Seq[Point[A]]) => Seq[Point[A]],
+                isCorePoint: (Point[A], Seq[Point[A]]) => Boolean): Seq[Seq[A]] = {
+    val gdbscan = new DBSCAN(getNeighbours, isCorePoint)
+    gdbscan.cluster(data).map(_.points.map(_.value))
+  }
+
+  /** Single point in the cluster */
+  case class Point[T](row: Int)(val value: T)
+
+  /** The cluster itself */
+  case class Cluster[T](id: Long) {
+    private var _points = ListBuffer[Point[T]]()
+
+    def add(p: Point[T]) {
+      _points += p
+    }
+
+    def points: Seq[Point[T]] = Seq(_points: _*)
+  }
+}
+
+/**
+ * A class used to run DBSCAN algorithm
+ * @param getNeighbours lists all point's neighbours
+ * @param isCorePoint determine if a given points is a core point among its neighbours
+ */
+class DBSCAN[T](
+                  getNeighbours: (Point[T], Seq[Point[T]]) => Seq[Point[T]],
+                  isCorePoint: (Point[T], Seq[Point[T]]) => Boolean) {
+
+  /**
+   * Performs the clustering.
+   *
+   * @param data data to be clustered
+   */
+  def cluster(data: Seq[T]): Seq[Cluster[T]] = {
+
+    val visited = MutableSet[Point[T]]()
+    val clustered = MutableSet[Point[T]]()
+
+    val points = for (row <- 0 until data.length) yield Point(row)(data(row))
+
+    points.collect {
+      case point@Point(row) if !(visited contains point) =>
+        val neighbours = getNeighbours(point, points.filterNot(_.row == point.row))
+        if (isCorePoint(point, neighbours)) {
+          visited add point
+          val cluster = Cluster[T](row)
+          expand(point, neighbours, cluster)(points, visited, clustered)
+          Some(cluster)
+        } else {
+          None
+        }
+    }.flatten
+  }
+
+  private def expand(point: Point[T], neighbours: Seq[Point[T]], cluster: Cluster[T])(implicit points: Seq[Point[T]], visited: MutableSet[Point[T]], clustered: MutableSet[Point[T]]) {
+    cluster.add(point)
+    clustered.add(point)
+
+    neighbours.foldLeft(neighbours) {
+      case (neighbourhood, neighbour@Point(row)) =>
+        val newNeighbours = if (!(visited contains neighbour)) {
+          visited.add(neighbour)
+          getNeighbours(neighbour, points.filterNot(_.row == neighbour.row))
+        } else {
+          Nil
+        }
+
+        if (!(clustered contains neighbour)) {
+          cluster.add(neighbour)
+          clustered.add(neighbour)
+        }
+
+        if (isCorePoint(neighbour, neighbourhood)) neighbourhood ++ newNeighbours
+        else neighbourhood
+    }
+  }
+}
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import java.io.File
 import java.nio.file.{Files, Paths}
 
+import _root_.DBSCAN.Point
 import com.typesafe.scalalogging.LazyLogging
 import scopt._
 
@@ -36,35 +37,18 @@ object Main extends App with LazyLogging {
 
   parser.parse(args.toSeq, Config("", 0)) match {
     case Some(config) =>
-      run(config)
+      run(dbscan)(config)
     case None =>
       // invalid args
   }
 
-  def distanceFunction: ((Image, Seq[Int]), (Image, Seq[Int])) => Double = { (a, b) =>
-    val differences = a._2.zip(b._2).map { case (v1, v2) =>
-      math.pow(v2 - v1, 2)
-    }
-    Math.sqrt(differences.sum)
-  }
-
-  def meanFunction(dataToSum: Seq[(Image, Seq[Int])]): (Image, Seq[Int]) = if (dataToSum.isEmpty)
-    (null, Seq.fill(768)(0))
-  else
-    (dataToSum.head._1, vectorMean(dataToSum.map(_._2)))
-
-  def vectorMean(vectors: Seq[Seq[Int]]): Seq[Int] =
-    for {
-    i <- vectors.head.indices
-  } yield vectors.map( v => v(i)).sum/vectors.size
-
-  def run(implicit config: Config) = {
+  def run(method: (Seq[(Image, Seq[Int])]) => Seq[Seq[(Image, Seq[Int])]])(implicit config: Config) = {
     if (Files.notExists(Paths.get(config.inputDir))) {
       logger.error(s"File ${config.inputDir} does not exist!")
     } else {
-      val files = Random.shuffle(listFiles(config.inputDir, config.pattern)).take(70)
+      val files = Random.shuffle(listFiles(config.inputDir, config.pattern)).take(20)
       logger.info(s"Found ${files.length} files")
-      val clusters = process(files)
+      val clusters = process(files, method)
 
       config.output.foreach { o =>
         ImagesSaver.saveImageClusters(clusters, o)
@@ -72,24 +56,12 @@ object Main extends App with LazyLogging {
     }
   }
 
-  def listFiles(directory: String, pattern: String = ".*"): Seq[String] = {
-    val d = new File(directory)
-    if (d.exists && d.isDirectory) {
-      d.listFiles
-        .filter(f => f.isFile && f.getName.matches(pattern))
-        .map(x => x.getAbsolutePath)
-        .sorted
-    } else {
-      Seq()
-    }
-  }
-
-  def process(inputFiles: Seq[String])(implicit config: Config): Seq[Seq[Image]] = {
+  private def process(inputFiles: Seq[String], cluster: (Seq[(Image, Seq[Int])]) => Seq[Seq[(Image, Seq[Int])]])(implicit config: Config): Seq[Seq[Image]] = {
     logger.info(s"Calculating histograms...")
-    val data = inputFiles.map(process).toIndexedSeq
+    val data = inputFiles.map(process)
 
     logger.info(s"Clustering...")
-    val result = KMeans[(Image, Seq[Int])](data, config.k, distanceFunction, config.minChangeInDispersion, config.maxNumberOfIterations, meanFunction)
+    val result = cluster(data)
 
     logger.debug(s"Result:")
     result.zipWithIndex.map { case (xa, i) => logger.info(s"Cluster ${i+1}: ${xa.map(_._1.name).mkString(", ")}") }
@@ -105,5 +77,53 @@ object Main extends App with LazyLogging {
     val histogram = image.hsvHistogram
     logger.debug("Image processed!")
     (image, histogram)
+  }
+
+  private def listFiles(directory: String, pattern: String = ".*"): Seq[String] = {
+    val d = new File(directory)
+    if (d.exists && d.isDirectory) {
+      d.listFiles
+        .filter(f => f.isFile && f.getName.matches(pattern))
+        .map(x => x.getAbsolutePath)
+        .sorted
+    } else {
+      Seq()
+    }
+  }
+
+
+  def distanceFunction: ((Image, Seq[Int]), (Image, Seq[Int])) => Double = { (a, b) =>
+    val differences = a._2.zip(b._2).map { case (v1, v2) =>
+      math.pow(v2 - v1, 2)
+    }
+    Math.sqrt(differences.sum)
+  }
+
+  private def kmeans(data: Seq[(Image, Seq[Int])])(implicit config: Config): Seq[Seq[(Image, Seq[Int])]] = {
+
+    def meanFunction(dataToSum: Seq[(Image, Seq[Int])]): (Image, Seq[Int]) = if (dataToSum.isEmpty)
+      (null, Seq.fill(768)(0))
+    else
+      (dataToSum.head._1, vectorMean(dataToSum.map(_._2)))
+
+    def vectorMean(vectors: Seq[Seq[Int]]): Seq[Int] =
+      for {
+        i <- vectors.head.indices
+      } yield vectors.map( v => v(i)).sum/vectors.size
+
+    KMeans[(Image, Seq[Int])](data.toIndexedSeq, config.k, distanceFunction, config.minChangeInDispersion, config.maxNumberOfIterations, meanFunction)
+  }
+
+  private def dbscan(data: Seq[(Image, Seq[Int])])(implicit config: Config): Seq[Seq[(Image, Seq[Int])]] = {
+
+    def getNeighbours(epsilon: Double, distance: ((Image, Seq[Int]), (Image, Seq[Int])) => Double)(point: Point[(Image, Seq[Int])], points: Seq[Point[(Image, Seq[Int])]]): Seq[Point[(Image, Seq[Int])]] = {
+      points.filter(neighbour => distance(neighbour.value, point.value) < epsilon)
+    }
+
+    def isCorePoint(minPoints: Int)(point: Point[(Image, Seq[Int])], neighbours: Seq[Point[(Image, Seq[Int])]]): Boolean = {
+      neighbours.size >= minPoints
+    }
+
+    DBSCAN[(Image, Seq[Int])](data, getNeighbours(10000, distanceFunction), isCorePoint(5))
   }
 }
