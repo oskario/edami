9a5ab7565da9135ab3155cfa59da628e0b24df9d
oskario <oskario2007@gmail.com>
2015-06-14 15:29:24 +0200

    Image saver fixed (not working on Linux)

--- src/main/scala/ImagesSaver.scala
+++ src/main/scala/ImagesSaver.scala
@@ -1,25 +1,34 @@
 import java.io.File
+import java.nio.file.{Paths, Files}
 import javax.imageio.ImageIO
+
+import com.typesafe.scalalogging.LazyLogging
 
 /**
  * Object used to save images.
  */
-object ImagesSaver {
+object ImagesSaver extends LazyLogging {
   /**
    * Saves image clusters creating one directory per cluster in the outputDir.
    * @param imageClusters image clusters
    * @param outputDir output dir
    */
   def saveImageClusters(imageClusters: Seq[Seq[Image]], outputDir: String) = {
-    var clusterNumber = 0
-    imageClusters.foreach(cluster => {
-      new File(s"$outputDir\\$clusterNumber").mkdir()
-      cluster.foreach(image => {
-        val outputfile = new File(s"$outputDir\\$clusterNumber\\${image.file.getName}")
-        ImageIO.write(image.image, "jpg", outputfile)
+    logger.info(s"Saving ${imageClusters.length} clusters to $outputDir")
+    val output = Paths.get(outputDir)
+    Files.deleteIfExists(output)
+    Files.createDirectory(output)
+
+    imageClusters.zipWithIndex.foreach { case (cluster, i) =>
+      val clusterNumber = i + 1
+      val clusterDir = Paths.get(outputDir + File.separator + clusterNumber)
+      Files.createDirectory(clusterDir)
+
+      cluster.foreach { image =>
+        val outputFilePath = outputDir + File.separator + clusterNumber + File.separator + image.file.getName
+        val outputFile = new File(outputFilePath)
+        ImageIO.write(image.image, "jpg", outputFile)
       }
-      )
-      clusterNumber = clusterNumber + 1
-    })
+    }
   }
 }
